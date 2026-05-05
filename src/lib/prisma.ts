e1599dfafeee925bf93c10bbada370e333eb42c0
Zach Harwood <320zach@gmail.com>
2026-05-04 23:13:56 -0400

    Update Prisma import to use WASM version and format code for consistency

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/wasm"
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
